11427a8dbba1f1714ed17234205630727841dd5a
sayaliph18 <110461833+sayaliph18@users.noreply.github.com>
2024-03-31 18:04:20 +0530

    Add files via upload

---
+++ TF-IDF.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import nltk
+
+
+# In[2]:
+
+
+paragraph =  """I have three visions for India. In 3000 years of our history, people from all over 
+               the world have come and invaded us, captured our lands, conquered our minds. 
+               From Alexander onwards, the Greeks, the Turks, the Moguls, the Portuguese, the British,
+               the French, the Dutch, all of them came and looted us, took over what was ours. 
+               Yet we have not done this to any other nation. We have not conquered anyone. 
+               We have not grabbed their land, their culture, 
+               their history and tried to enforce our way of life on them. 
+               Why? Because we respect the freedom of others.That is why my 
+               first vision is that of freedom. I believe that India got its first vision of 
+               this in 1857, when we started the War of Independence. It is this freedom that
+               we must protect and nurture and build on. If we are not free, no one will respect us.
+               My second vision for India’s development. For fifty years we have been a developing nation.
+               It is time we see ourselves as a developed nation. We are among the top 5 nations of the world
+               in terms of GDP. We have a 10 percent growth rate in most areas. Our poverty levels are falling.
+               Our achievements are being globally recognised today. Yet we lack the self-confidence to
+               see ourselves as a developed nation, self-reliant and self-assured. Isn’t this incorrect?
+               I have a third vision. India must stand up to the world. Because I believe that unless India 
+               stands up to the world, no one will respect us. Only strength respects strength. We must be 
+               strong not only as a military power but also as an economic power. Both must go hand-in-hand. 
+               My good fortune was to have worked with three great minds. Dr. Vikram Sarabhai of the Dept. of 
+               space, Professor Satish Dhawan, who succeeded him and Dr. Brahm Prakash, father of nuclear material.
+               I was lucky to have worked with all three of them closely and consider this the great opportunity of my life. 
+               I see four milestones in my career"""
+               
+               
+
+
+# In[3]:
+
+
+#Cleaning the text
+import re
+
+
+# In[4]:
+
+
+from nltk.corpus import stopwords
+
+
+# In[5]:
+
+
+from nltk.stem.porter import PorterStemmer
+
+
+# In[6]:
+
+
+from nltk.stem import WordNetLemmatizer
+
+
+# In[7]:
+
+
+ps = PorterStemmer()
+
+
+# In[8]:
+
+
+wordnet = WordNetLemmatizer()
+
+
+# In[9]:
+
+
+sentences = nltk.sent_tokenize(paragraph)
+
+
+# In[11]:
+
+
+corpus = []
+for i in range(len(sentences)):
+    review = re.sub('[^a-zA-Z]', ' ', sentences[i])
+    review = review.lower()
+    review = review.split()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+
+# In[13]:
+
+
+#cerating the Bag of words Model
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+# In[14]:
+
+
+cv = TfidfVectorizer()
+X = cv.fit_transform(corpus).toarray()
+
+
+# In[ ]:
+
+
+
+
